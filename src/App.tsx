4e957b0a0d041d4128dc6bf3f7ca2dfa105c35b0
Hasan Mohamed Hasan <h01554176846@gmail.com>
2024-10-26 23:52:16 +0300

    2024-10-26 11:52:16 PM

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,18 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Footer from "./assets/components/Footer";
+import { Helmet } from "react-helmet";
 
 const App = () => {
   return (
     <div className="bg-theme1-dark">
+      <Helmet>
+        <title>hasan ragab</title>
+        <meta name="description" content="hasan ragab web dev" />
+        <meta name="keywords" content="developer, hasan, ragab" />
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="https://hasan-ragab.vercel.app/" />
+      </Helmet>
       <Navbar />
       <div>
         <Home />
